ba64e335fdc3d63fb7582962311af52339561d49
adamnfish <adamnfish@gmail.com>
2024-12-03 20:56:09 +0000

    Day 3 puzzle 2

--- src/main/scala/com/adamnfish/days/Day03.scala
+++ src/main/scala/com/adamnfish/days/Day03.scala
@@ -11,7 +11,7 @@ object Day03:
   def part1(inputFile: String) =
     Tools
       .inputLines("3", inputFile)
-      .evalMap(Parser.parseLine[IO])
+      .evalMap(Part1.Parser.parseLine[IO])
       .compile
       .toList
       .map { parsedLines =>
@@ -21,21 +21,75 @@ object Day03:
       }
 
   def part2(inputFile: String) =
-    Tools.inputLines("3", inputFile).compile.lastOrError
+    Tools
+      .inputLines("3", inputFile)
+      .evalMap(Part2.Parser.parseLine[IO])
+      .compile
+      .toList
+      .map { parsedLines =>
+        val operations = parsedLines.flatten
+        Part2.runOperations(operations)
+      }
 
-  object Parser:
-    import fastparse.*
-
-    def parseLine[F[_]: MonadThrow](line: String): F[List[(Int, Int)]] =
-      parse(line, lineParser(using _)).intoF
-
-    private def lineParser[$: P]: P[List[(Int, Int)]] =
+  object Part1:
+    object Parser:
+      import fastparse.*
       import MultiLineWhitespace.*
-      P(
-        (!multParser ~ AnyChar).rep ~
-          multParser.rep(sep = (!multParser ~ AnyChar).rep)
-      ).map(_.toList)
 
-    private def multParser[$: P]: P[(Int, Int)] =
-      import NoWhitespace.*
-      P("mul(" ~ integer ~ "," ~ integer ~ ")")
+      def parseLine[F[_]: MonadThrow](line: String): F[List[(Int, Int)]] =
+        parse(line, lineParser(using _)).intoF
+
+      private def lineParser[$: P]: P[List[(Int, Int)]] =
+        P(
+          (!multParser ~ AnyChar).rep ~
+            multParser.rep(sep = (!multParser ~ AnyChar).rep)
+        ).map(_.toList)
+
+      private def multParser[$: P]: P[(Int, Int)] =
+        P("mul(" ~ integer ~ "," ~ integer ~ ")")
+
+  object Part2:
+    enum Operation:
+      case Do()
+      case Dont()
+      case Mult(n1: Int, n2: Int)
+
+    def runOperations(operations: List[Operation]): Int =
+      val (result, _) = operations.foldLeft((0, true)) {
+        case ((total, _), Operation.Do()) =>
+          (total, true)
+        case ((total, _), Operation.Dont()) =>
+          (total, false)
+        case ((total, false), Operation.Mult(_, _)) =>
+          (total, false)
+        case ((total, true), Operation.Mult(n1, n2)) =>
+          (total + n1 * n2, true)
+      }
+      result
+
+    object Parser:
+      import fastparse.*
+      import MultiLineWhitespace.*
+
+      def parseLine[F[_]: MonadThrow](line: String): F[List[Operation]] =
+        parse(line, lineParser(using _)).intoF
+
+      private def lineParser[$: P]: P[List[Operation]] =
+        P(
+          (!operationParser ~ AnyChar).rep ~
+            operationParser.rep(sep = (!operationParser ~ AnyChar).rep)
+        ).map(_.toList)
+
+      private def operationParser[$: P]: P[Operation] =
+        P(multParser | doParser | dontParser)
+
+      private def multParser[$: P]: P[Operation.Mult] =
+        P("mul(" ~ integer ~ "," ~ integer ~ ")").map { case (n1, n2) =>
+          Operation.Mult(n1, n2)
+        }
+
+      private def doParser[$: P]: P[Operation.Do] =
+        P("do()").map(_ => Operation.Do())
+
+      private def dontParser[$: P]: P[Operation.Dont] =
+        P("don't()").map(_ => Operation.Dont())
